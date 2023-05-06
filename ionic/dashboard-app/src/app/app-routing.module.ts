9a5777287ce946ec31134c2c3fd62f395ad9c3e9
Franziska Furtado <franziskafurtado@gmail.com>
2023-05-06 17:27:48 -0300

    interface feita com ionic
    ainda não terminei

---
+++ ionic/dashboard-app/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'principal',
+    pathMatch: 'full'
+  },
+  {
+    path: 'principal',
+    loadChildren: () => import('./pages/principal/principal.module').then( m => m.PrincipalPageModule)
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
