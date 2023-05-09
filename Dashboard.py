a418755f79291a5fc125acb36ec6ba09d8e602f2
PHSecchi <pedrohsecchi@gmail.com>
2023-05-09 17:14:35 -0300

    Implementacao da percetagem do uso de CPU e Filtragem de processos apenas para usuario logado

--- Dashboard.py
+++ Dashboard.py
@@ -10,6 +10,23 @@ def aquisicao_hostname():
     
     return {"Hostname": hostname}
 
+def aquisicao_dados_memo():
+    with open('/proc/meminfo', 'r') as arquivo:
+        info_memo = arquivo.readlines()
+
+    for m in info_memo:
+        if m.startswith('MemTotal:'):
+                memo_total = int(m.split()[1])
+        if m.startswith('MemFree:'):
+                memo_free = int (m.split()[1])
+    
+    memo_uso = memo_total-memo_free
+
+    return{"Total": memo_total,
+           "Free" : memo_free,
+           "emUso": memo_uso
+            }
+
 def aquisicao_dados_cpu():
     
     with open('/proc/cpuinfo', 'r') as arquivo:
@@ -19,44 +36,33 @@ def aquisicao_dados_cpu():
         modelo = info_CPU[4].split('\t')[1].split(' ',1)[1].strip()
         cores = info_CPU[12].split('\t')[1].split(' ')[1].strip()
         threads = info_CPU[10].split('\t')[1].split(' ')[1].strip()
-
-        cpu = {"Modelo": modelo,
-               "Nucleos Fisicos": cores,
-               "Nucleos Virtuais" : threads }
-        dados_CPU = { "CPU" : cpu}
-       
- 
-    CPU_stat = []
-    i=0
-    t_cpu = []
+        
     #Carrega as informacoes sobre o tempo do processador
     with open('/proc/stat', 'r') as arquivo:
-        while i < int(threads) + 1 :
-            CPU_stat.append(arquivo.readline())
-            j=0
-            if i == 0:
-                j=1 
-            tempos = []
-            k = 1
-            while k < 11:
-                tempos.append(CPU_stat[i].split(' ')[k+j].strip())
-                k += 1
+        stat_cpu1 = arquivo.readline()
+        
+    time.sleep(1)
+        
+    with open('/proc/stat', 'r') as arquivo:
+        stat_cpu2 = arquivo.readline()
+        
+        t_cpu1 = []
+        t_cpu2 = []
+        k = 2
+        while k < 12:
+            t_cpu1.append(int(stat_cpu1.split(' ')[k].strip()))
+            t_cpu2.append(int(stat_cpu2.split(' ')[k].strip()))
+            k += 1
 
-            t_cpu.append({"t_user1":int(tempos[0]),
-                          "t_user2":int(tempos[1]),
-                          "t_system":int(tempos[2]),
-                          "t_ocioso":int(tempos[3]),
-                          "t_i/o_wait":int(tempos[4]),
-                          "t_inthard":int(tempos[5]),
-                          "t_intsoft":int(tempos[6]),
-                          "t_virt":int(tempos[7]),
-                          "t_virt1cpu":int(tempos[8]),
-                          "t_energ":int(tempos[9])})
-            i += 1
+        t_cpu1total = sum(t_cpu1)
+        t_cpu2total = sum(t_cpu2)
 
-    dados_CPU = { "info_CPU" : cpu,
-                 "times_CPU": t_cpu}
-    return dados_CPU 
+        percent = ((t_cpu2total - t_cpu2[3])-(t_cpu1total - t_cpu1[3])) / (t_cpu2total - t_cpu1total) * 100
+
+    return {"Modelo": modelo,
+            "Nucleos Fisicos": cores,
+            "Nucleos Virtuais" : threads,
+            "Porcentagem de Uso": percent }
  
 
 def aquisicao_dados_processos():
@@ -73,35 +79,39 @@ def aquisicao_dados_processos():
             status = arquivo.readlines()
             memo = 0
         for s in status:
-            if (s.split('\t')[0].strip()) == 'Pid:':
+            if s.startswith('Pid:'):
                 pid = s.split('\t')[1].strip() 
-            if (s.split('\t')[0].strip()) == 'PPid:':
+            if s.startswith('PPid:'):
                 ppid = s.split('\t')[1].strip() 
-            if (s.split('\t')[0].strip()) == 'Name:':
+            if s.startswith('Name:'):
                 name = s.split('\t')[1].strip() 
-            if (s.split('\t')[0].strip()) == 'VmSize:':
+            if s.startswith('VmSize:'):
                 memo = s.split('\t')[1].strip() 
-            if (s.split('\t')[0].strip()) == 'Threads:':
+            if s.startswith('Threads:'):
                 trd = s.split('\t')[1].strip() 
-            if (s.split('\t')[0].strip()) == 'Uid:':
-                uid = s.split('\t')[1].strip() 
+            if s.startswith('Uid:'):
+                uid = int(s.split('\t')[1].strip()) 
 
-        user = pwd.getpwuid(int(uid)).pw_name
+        with open('/proc/self/loginuid', 'r') as arquivo:
+            loginuid = int(arquivo.read().strip())
 
-        #Monta uma string e concatena com as informacoes dos demais processos
-        proc.append ({"PID": pid,
-                      "PPID": ppid,
-                      "Nome": name,
-                      "Ususario": user, 
-                      "Memoria": memo,
-                      "Quantidade de Threads": trd})
+        if loginuid == uid :
+            user = pwd.getpwuid(uid).pw_name
+            proc.append ({"PID": pid,
+                          "PPID": ppid,
+                          "Nome": name,
+                          "Ususario": user, 
+                          "Memoria": memo,
+                          "Quantidade de Threads": trd})
 
     return proc
 
 def grava_dados():
-    while True: 
+    while True:
+        #aquisicao_dados_cpu() 
         Dados = {"Hostname": aquisicao_hostname(),
                  "CPU":aquisicao_dados_cpu(),
+                 "Memoria": aquisicao_dados_memo(),
                  "Processos":aquisicao_dados_processos()}
         # Mostra as informações na tela
         #print(aquisicao_dados_processos())
